fe0747d9be3253c31d382fc3a2c0f6b367c909ca
ags-mites <eamites1@espe.edu.ec>
2024-08-03 10:16:36 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '2.2_Manejo_de_listas_frameworks',
+  webDir: 'dist'
+};
+
+export default config;
